122e2ef771594583d428b4164cb7f1f2ad7e2745
João Oliveira <joao.ol.dev@gmail.com>
2025-11-28 10:50:19 -0300

    url-fix

--- sustaina-diagnosis-backend/app/main.py
+++ sustaina-diagnosis-backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Diagnóstico de Sustentabilidade")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://sustaina-diagnosis.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
